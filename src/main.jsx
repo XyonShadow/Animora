f8704832f5422b93cd0c2ac2e688e14e375f843f
Michael <prncemk11@gmail.com>
2026-05-19 16:47:47 +0100

    install react-router-dom and setup BrowserRouter in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+/**
+ * main.jsx
+ *
+ * Changes:
+ *  - Wrapped the entire app with <BrowserRouter> to enable React Router
+ *  - Enables route handling for the entire application
+ */
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
